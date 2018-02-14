ea6bef90d48c85857e526078e364f657e5dc0a6b
suzhou-li <suzhou.li@duke.edu>
2018-02-13 19:48:52 -0500

    Adds the function find_segments.m to find each connected component in the image and highlights each connected component with a blue bounding box.

---
+++ matlab-api/suzhou/find_segments.m
@@ -0,0 +1,104 @@
+function [imgCC, outPic] = find_segments(inPic)
+% [imgCC, outPic] = find_segments(inPic)
+%
+% Function to find different connected segments in the picture.
+%
+% Inputs:
+% - inPic = matrix containing the pixel values for the RGB or grayvalue
+%           images
+%
+% Outputs:
+% - imgCC = connected components structure containing the following
+%           parameters:
+%   - Area = actual number of pixels in the region
+%   - BoundingBox = rectangle coordinates containing the region of the
+%                   connected components in the image
+%   - Centroid = center of mass of the region of the connected components
+%                in the image
+% - outPic = matrix containing the logic values of the binarized image 
+%
+% Written by:
+% Suzhou Li
+
+    %% Make the image grayscale
+    if (size(inPic, 3) ~= 1)
+        inPic = rgb2gray(inPic);
+    end
+    
+    %% Crop the image
+    inPic = imcrop(inPic);
+    close;
+    
+    %% Binarize and smooth the image
+    midPic = block_binarize(inPic, [2 2] .^ 6);
+    midPic = imclose(~midPic, strel('disk', 5));
+    midPic = bwareaopen(midPic, 20);
+    outPic = midPic;
+    
+    %% Find the different segments
+    connected = bwconncomp(midPic);
+    imgCC = regionprops(connected, ...
+        'Area', 'BoundingBox', 'Centroid', 'Image');
+    
+    %% Show the labeled image
+    
+    %  Show the binarized image
+    imshow(~outPic);
+    
+    %  Plot the rectangle boundaries
+    hold on
+    for cc = 1 : numel(imgCC)
+        rectangle('Position', imgCC(cc).BoundingBox, 'EdgeColor', 'b');
+    end
+    hold off
+    
+    %  Skip the rest of the code
+    return;
+    
+    %% Self made connected component finding code (probably no good)
+    
+    %  Pad the matrix with 0's
+    midPic = [true(size(midPic, 1), 1), midPic, true(size(midPic, 1), 1)];
+    midPic = [true(1, size(midPic, 2)); midPic; true(1, size(midPic, 2))];
+    
+    %  Initialize the variables
+    outPic = false(size(midPic)); % matrix to hold segment values
+    curr_seg = 0; % initialize the current segment value
+    
+    %  Iterate through the image
+    for row = 2 : size(midPic, 1) - 1
+        for col = 2 : size(midPic, 2) - 1
+            
+            % If there is an element at the pixel
+            if (midPic(row, col) == 0)
+                
+                % Get the box of neighboring pixel values and neighboring
+                % currently found segments
+                box = midPic(row - 1 : row + 1, col - 1 : col + 1);
+                box(2, 2) = 0;
+                seg_box = outPic(row - 1 : row + 1, col - 1 : col + 1);
+                
+                % If there are neighboring pixels
+                if any(box(:) == 0)
+                    
+                    % If there is a segment in the surrounding box
+                    if any(seg_box(:)) 
+                        seg = seg_box(seg_box ~= 0);
+                        seg = seg(1);
+                        
+                    % If there has not been a segment found in the
+                    % surrounding box
+                    else
+                        curr_seg = curr_seg + 1;
+                        seg = curr_seg;
+                    end
+                    
+                    % Name the pixel as the specified segment
+                    outPic(row, col) = seg;
+                end
+            end
+        end
+    end
+    
+    outPic = outPic(2 : size(inPic, 1) + 1, 2 : size(inPic, 2) + 1);
+end
--- matlab-api/suzhou/testCode.m
+++ matlab-api/suzhou/testCode.m
@@ -2,17 +2,25 @@
 clear, close all;
 
 %% Load the transform
-imgRGB = imread('../../img/res_cap1.jpg');
+imgRGB = imread('../../img/alphabet_li_1.jpg'); 
+%imgRGB = imread('../../img/res_cap1.jpg');
 imgGray = rgb2gray(imgRGB);
+
+%% Find the segments in the image
+[imgCC, imgBW] = find_segments(imgGray);
+return;
 
 %% Process the image
 
+%  Crop out the unnecessary parts of the image
+imgMid = imcrop(imgGray);
+
 %  Median filter to denoise (optional)
-imgMid = medfilt2(imgGray);
+imgMid = medfilt2(imgMid);
 figure, clf;
 imshow(imgMid);
 
 %  Binarize the image using blocks of 256x256 pixels
-imgBW = block_binarize(imgMid, [2 2] .^ 8);
+imgBW = block_binarize(imgMid, [2 2] .^ 4);
 figure, clf;
 imshow(imgBW);
