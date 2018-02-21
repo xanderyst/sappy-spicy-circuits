af03564421e7327ff82b954038012c10afee86a0
Cristina Lai <cristinalai@Cristinas-MacBook-Pro.local>
2018-02-20 21:53:29 -0500

    trying new feature extraction function

--- cris_test.m
+++ cris_test.m
@@ -4,15 +4,26 @@
 %     figure(i); 
 %     imshow(crismat)
 %     filename = sprintf('%s_%d.jpg', 'image', i);
-%     filename = strcat('data/cris_img/', filename);
+%     if cris.Y(i) == 0
+%         filename = strcat('data/cris_img/resistor/res', filename);
+%     end
+%     if cris.Y(i) == 1
+%         filename = strcat('data/cris_img/capacitor/cap', filename);
+%     end
 %     saveas(gcf, filename);
 % end
 
 %% image data store works??
-% imds = imageDatastore('data/cris_img', 'IncludeSubFolders', true, 'FileExtensions', '.jpg', 'LabelSource', 'foldernames')
+imds = imageDatastore('data/cris_img', 'IncludeSubFolders', true, 'FileExtensions', '.jpg', 'LabelSource', 'foldernames')
 
 %% gimme a bag of features
 [train, validate] = splitEachLabel(imds, 0.8, 'randomize');
-bag = bagOfFeatures(train);
-catClass = trainImageCategoryClassifier(train, bag);
+% bag = bagOfFeatures(train);
+
+%% can i modify this bag
+cornerFcn = @cornerFeatureExtractor;
+
+corner_bag = bagOfFeatures(train, 'CustomExtractor', cornerFcn);
+%% test it
+catClass = trainImageCategoryClassifier(train, corner_bag);
 confMatrix = evaluate(catClass, train);
