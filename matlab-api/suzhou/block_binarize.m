0c4ec97f60b84b1ceb6eb21f550e647732673695
suzhou-li <suzhou.li@duke.edu>
2018-03-23 16:24:39 -0400

    Added an extra argument to block_binarize.m to allow the user to decide what threshold of binarization to ignore.

--- matlab-api/suzhou/block_binarize.m
+++ matlab-api/suzhou/block_binarize.m
@@ -1,5 +1,5 @@
-function outPic = block_binarize(inPic, blockSize, conf)
-% outPic = block_binarize(inPic, blockSize, conf_thresh)
+function outPic = block_binarize(inPic, blockSize, conf, thresh)
+% outPic = block_binarize(inPic, blockSize, conf, thresh)
 % 
 % Binarizes the image in blocks.
 % 
@@ -17,25 +17,26 @@ function outPic = block_binarize(inPic, blockSize, conf)
     % Check the number of input arguments
     if (nargin <= 2)
         conf = 0.75;
+        thresh = 0.75;
     end
     
     % Use block process to divide the image into blocks and binarize each
     % block of the image
-    block_fun = @(block_struct) process_block(block_struct.data, conf);
+    block_fun = @(block_struct) process_block(block_struct.data, conf, thresh);
     outPic = blockproc(inPic, blockSize, block_fun, ...
         'UseParallel', true);
 end
 
-function outBlock = process_block(inBlock, conf)
+function outBlock = process_block(inBlock, conf, thresh)
 
     % Obtain the threshold using Otsu's algorithm
-    [thresh, eff] = graythresh(inBlock);
+    [th, eff] = graythresh(inBlock);
     
     % Check if the confidence is too low or the threshold is too high
-    if ((eff < conf) || (thresh > 0.6))
-        thresh = 0;
+    if ((eff < conf) || (th > thresh))
+        th = 0;
     end
     
     % Return the binarized block
-    outBlock = imbinarize(inBlock, thresh);
+    outBlock = imbinarize(inBlock, th);
 end
