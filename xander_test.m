c6c749322959dd8a8e09390199ccc909c71ab4b7
Alexander Tseng <xanderyst@gmail.com>
2018-04-05 03:13:49 -0400

    Added ACF and Cascade object detectors

---
+++ xander_test.m
@@ -0,0 +1,22 @@
+resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
+resGroundTruth = selectLabels(resAndCapGroundTruth.gTruth,'resistor');
+trainingData = objectDetectorTrainingData(resGroundTruth);
+acfDetector = trainACFObjectDetector(trainingData,'NegativeSamplesFactor',2);
+I = imread('data/xan_test/testing/testing2.jpg');
+
+[bboxes, scores] = detect(acfDetector, img);
+
+for i = 1:length(scores)
+   if (scores(i)>70.0)
+       sprintf('%.1f\n', scores(i))
+       disp(bboxes(i,:));
+       annotation = sprintf('Confidence = %.1f', scores(i));
+       img = insertObjectAnnotation(img, 'rectangle', bboxes(i,:), annotation);
+   end
+end
+
+figure
+imshow(img)
+
+
+
