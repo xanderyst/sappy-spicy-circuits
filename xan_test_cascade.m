d8658f7f815f38800122a8d825e99a57a9d82e48
Alexander Tseng <xanderyst@gmail.com>
2018-04-05 04:10:17 -0400

    Added comments

--- xan_test_cascade.m
+++ xan_test_cascade.m
@@ -1,5 +1,5 @@
-resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
-%resAndCapGroundTruth = load('data/xan_test/groundTruth_resOnly.mat');
+%resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
+resAndCapGroundTruth = load('data/xan_test/groundTruth_resCap.mat');
 resGroundTruth = selectLabels(resAndCapGroundTruth.gTruth,'resistor');
 trainingData = objectDetectorTrainingData(resGroundTruth);
 
@@ -9,7 +9,7 @@ trainCascadeObjectDetector('resDetector.xml',trainingData, ...
     negativeFolder,'FalseAlarmRate',0.1,'NumCascadeStages',7);
 
 detector = vision.CascadeObjectDetector('resDetector.xml');
-img = imread('data/xan_test/testing/testing2.jpg');
+img = imread('data/xan_test/testing/testing.jpg');
 
 bbox = step(detector,img);
 detectedImg = insertObjectAnnotation(img,'rectangle',bbox,'resistor');
--- xander_test.m
+++ xander_test.m
@@ -1,15 +1,16 @@
-resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
-resGroundTruth = selectLabels(resAndCapGroundTruth.gTruth,'resistor');
+%resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
+resAndCapGroundTruth = load('data/xan_test/groundTruth_resCap.mat');
+resGroundTruth = selectLabels(resAndCapGroundTruth.gTruth,'capacitor');
 trainingData = objectDetectorTrainingData(resGroundTruth);
 acfDetector = trainACFObjectDetector(trainingData,'NegativeSamplesFactor',2);
-I = imread('data/xan_test/testing/testing2.jpg');
+img = imread('data/xan_test/testing/testing2.jpg');
 
 [bboxes, scores] = detect(acfDetector, img);
 
 for i = 1:length(scores)
-   if (scores(i)>70.0)
-       sprintf('%.1f\n', scores(i))
-       disp(bboxes(i,:));
+   if (scores(i)>80.0) %use 60 for resistor and 80 for capacitors
+       %sprintf('%.1f\n', scores(i))
+       %disp(bboxes(i,:));
        annotation = sprintf('Confidence = %.1f', scores(i));
        img = insertObjectAnnotation(img, 'rectangle', bboxes(i,:), annotation);
    end
