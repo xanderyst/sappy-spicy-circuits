ac3f174eac447722b975187ce51881ffc6fdc8c3
Alexander Tseng <xanderyst@gmail.com>
2018-04-05 02:56:14 -0400

    Tried ACR and Cascade object detector, reorganized the images

--- xan_test_cascade.m
+++ xan_test_cascade.m
@@ -1,14 +1,15 @@
 resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
+%resAndCapGroundTruth = load('data/xan_test/groundTruth_resOnly.mat');
 resGroundTruth = selectLabels(resAndCapGroundTruth.gTruth,'resistor');
 trainingData = objectDetectorTrainingData(resGroundTruth);
 
 negativeFolder = fullfile('data','xan_test','noRes');
 negativeImages = imageDatastore(negativeFolder);
 trainCascadeObjectDetector('resDetector.xml',trainingData, ...
-    negativeFolder,'FalseAlarmRate',0.08,'NumCascadeStages',4);
+    negativeFolder,'FalseAlarmRate',0.1,'NumCascadeStages',7);
 
 detector = vision.CascadeObjectDetector('resDetector.xml');
-img = imread('data/xan_test/testing6.jpg');
+img = imread('data/xan_test/testing/testing2.jpg');
 
 bbox = step(detector,img);
 detectedImg = insertObjectAnnotation(img,'rectangle',bbox,'resistor');
--- xander_test.m
+++ xander_test.m
@@ -2,9 +2,20 @@ resAndCapGroundTruth = load('data/xan_test/groundTruth.mat');
 resGroundTruth = selectLabels(resAndCapGroundTruth.gTruth,'resistor');
 trainingData = objectDetectorTrainingData(resGroundTruth);
 acfDetector = trainACFObjectDetector(trainingData,'NegativeSamplesFactor',2);
-I = imread('data/xan_test/testing3.jpg');
-bboxes = detect(acfDetector,I);
-annotation = acfDetector.ModelName;
-I = insertObjectAnnotation(I,'rectangle',bboxes,annotation);
-figure 
-imshow(I)
+I = imread('data/xan_test/testing/testing2.jpg');
+
+[bboxes, scores] = detect(acfDetector, img);
+
+for i = 1:length(scores)
+   if (scores(i)>85.0)
+       sprintf('%.1f\n', scores(i))
+       annotation = sprintf('Confidence = %.1f', scores(i));
+       %img = insertObjectAnnotation(img, 'rectangle', bboxes(i,:), annotation);
+   end
+end
+
+figure
+imshow(img)
+
+
+
